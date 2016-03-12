3c6b2529c11be5168fb765a6ec4f92a076333e95
qweasd7924 <pavel.pugachev.94@mail.ru>
2016-03-12 13:30:17 +0300

    working project

---
+++ NewUser/src/main/java/ru/ncedu/entity/User.java
@@ -0,0 +1,80 @@
+package ru.ncedu.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * Created by Gamzat on 03.12.2015.
+ */
+@Entity(name = "users")
+@NamedQueries({
+        @NamedQuery(name = "User.getAllUsers", query = "SELECT u from users u"),
+        @NamedQuery(name = "User.getUserByLogin", query = "SELECT u from users u WHERE u.userName = :userName")
+    }
+)
+
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Integer id;
+
+    @Column
+    String userName;
+    @Column
+    String password;
+    @Column
+    String email;
+
+    @OneToOne(mappedBy="users")
+    private UserType userType;
+
+    public User() {
+    }
+
+    public User(String userName, String password, String email) {
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id = " + id+ " /" +
+                "userName = " +userName + " /" +
+                "email = "+email + " /" +
+                "}";
+    }
+}
---
+++ NewUser/src/main/java/ru/ncedu/service/UserService.java
@@ -0,0 +1,58 @@
+package ru.ncedu.service;
+
+import com.sun.istack.internal.Nullable;
+import ru.ncedu.entity.User;
+import ru.ncedu.entity.UserType;
+
+import javax.ejb.Stateless;
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * Created by Gamzat on 03.12.2015.
+ */
+
+@Stateless
+public class UserService {
+    @PersistenceUnit(unitName="NCEDU")
+    public static EntityManager em = Persistence.createEntityManagerFactory("NCEDU").createEntityManager();
+
+    public static List<User> getAllUsers() {
+        return em.createNamedQuery("User.getAllUsers", User.class).getResultList();
+    }
+
+    public static List<UserType> getAllTypes(){
+        return em.createNamedQuery("UserType.getTypes", UserType.class).getResultList();
+    }
+
+    @Nullable
+    public static User getUserByName(String userName) {
+        TypedQuery<User> query = em.createNamedQuery("User.getUserByLogin", User.class);
+        query.setParameter("userName", userName);
+
+        User user = null;
+            try{
+                user = query.getSingleResult();
+            }catch (NoResultException ignore){
+            }
+
+        if (user == null){
+            return null;
+        }
+        return user;
+    }
+
+    public static User addUser(User user){
+        em.getTransaction().begin();
+        User result = em.merge(user);
+        em.getTransaction().commit();
+        return result;
+    }
+
+    public static UserType addUserType(UserType uT){
+        em.getTransaction().begin();
+        UserType result = em.merge(uT);
+        em.getTransaction().commit();
+        return result;
+    }
+}
