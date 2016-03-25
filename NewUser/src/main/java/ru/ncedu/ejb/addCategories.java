db1e400f830a778d2ec309c9e635df9f48981ce9
Pavel <pavel.pugachev.94@mail.ru>
2016-03-25 20:49:57 +0300

    add test categories

--- NewUser/src/main/java/ru/ncedu/ejb/addCategories.java
+++ NewUser/src/main/java/ru/ncedu/ejb/addCategories.java
@@ -3,11 +3,13 @@ package ru.ncedu.ejb;
 import ru.ncedu.bean.CategoriesB;
 import ru.ncedu.bean.MarketManager;
 
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 /**
  * Created by Павел on 23.03.2016.
  */
+@Stateless
 public class addCategories {
     @Inject
     MarketManager marketManager;
